acd058c84b6c96c8fb036089ddfa43a1ca769f80
Geoff Hotchkiss <geoff@geoffhotchkiss.com>
2025-04-30 21:24:55 -0500

    enable italics in comment

--- init.lua
+++ init.lua
@@ -879,7 +879,7 @@ require('lazy').setup({
       ---@diagnostic disable-next-line: missing-fields
       require('tokyonight').setup {
         styles = {
-          comments = { italic = false }, -- Disable italics in comments
+          comments = { italic = true },
         },
       }
 
